4a82c3ce1e9169d10774843778024e654a96e564
Phnky <phunky@thephunkiest.com>
2018-04-27 17:24:10 -0700

    stop button; current time display

--- pomodoro-script.js
+++ pomodoro-script.js
@@ -1,11 +1,23 @@
 var d;
 var startTime;
 var currentTime = d;
+var clockRunning = false;
 
 //functions
+function clockRun(){
+    if(clockRunning == true){
+        updateClock();
+        currentTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
+        document.getElementById("current-time-display").innerHTML = currentTime.join(":");
+    }
+    setTimeout(clockRun,1000);
+}
+
 function startClock(){
     startTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
     document.getElementById("start-time-display").innerHTML = startTime.join(":");
+    clockRunning = true;
+    clockRun();
 }
 
 function updateClock(){
@@ -22,3 +34,4 @@ document.getElementById("start-button").onclick = function(){
     updateClock();
     startClock();
 };
+document.getElementById("stop-button").onclick = function () { clockRunning = false; };
