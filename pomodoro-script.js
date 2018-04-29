c19b8971b793d3dab56e9b87d607d35245fc30c9
Phnky <10679582+Phnky@users.noreply.github.com>
2018-04-29 16:04:59 -0700

    added display for the amount of time left

--- pomodoro-script.js
+++ pomodoro-script.js
@@ -4,6 +4,7 @@ var currentTime = d;
 var clockRunning = false;
 var timeElapsed = [0,0,0];
 var timeElapsedRaw = 0;
+var timerInitial = 300; //300 = 5 Minutes
 
 
 //functions
@@ -13,8 +14,9 @@ function clockRun(){
         currentTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
         document.getElementById("current-time-display").innerHTML = currentTime.join(":");
         updateTimeElapsed();
+        updateTimeLeft();
     }
-    setTimeout(clockRun,1000);
+    setTimeout(clockRun,1000); //runs once a second
 }
 
 function startClock(){
@@ -22,8 +24,11 @@ function startClock(){
     document.getElementById("start-time-display").innerHTML = startTime.join(":");
     document.getElementById("current-time-display").innerHTML = startTime.join(":");
     document.getElementById("time-elapsed-display").innerHTML = "0:0:0";
+    timeElapsedRaw = 0;
     clockRunning = true;
 }
+
+
 
 function updateClock(){
     d = new Date();
@@ -31,12 +36,26 @@ function updateClock(){
 
 function updateTimeElapsed(){
     var hours, minutes, seconds;
-    timeElapsedRaw ++;
+    timeElapsedRaw ++; //increment every second
     timeElapsed[2] = timeElapsedRaw%60;
     timeElapsed[1] = Math.floor(timeElapsedRaw/60);
     console.log(timeElapsedRaw);
     console.log(timeElapsed);
     document.getElementById("time-elapsed-display").innerHTML = timeElapsed.join(":");
+}
+
+function updateTimeLeft(){
+  var timerCalc = timerInitial - timeElapsedRaw;
+  var timerFormat = [0,0,0];
+
+  timerFormat[2] = timerCalc%60;
+  timerFormat[1] = Math.floor(timerCalc/60);
+  timerFormat[0] = Math.floor(timerCalc/3600);
+  document.getElementById("time-left-display").innerHTML = timerFormat.join(":");
+  if(timerCalc <= 0){
+    clockRunning = false;
+    console.log("Time up.");
+  }
 }
 
 //onload
@@ -49,9 +68,11 @@ window.onload = function(){
 document.getElementById("reset-button").onclick = function(){
   timeElapsedRaw = 0;
   timeElapsed = [0,0,0];
+  clockRunning = false;
   document.getElementById("start-time-display").innerHTML = "";
   document.getElementById("current-time-display").innerHTML = "";
   document.getElementById("time-elapsed-display").innerHTML= timeElapsed.join(":");
+  document.getElementById("time-left-display").innerHTML = "0:0:0";
 };
 document.getElementById("start-button").onclick = function(){
     updateClock();
