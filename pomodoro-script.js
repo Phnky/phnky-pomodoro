93a1f7fd285e55620ceab3d6a4d63b3052b1895c
Phnky <10679582+Phnky@users.noreply.github.com>
2018-04-29 16:52:45 -0700

    formatting

--- pomodoro-script.js
+++ pomodoro-script.js
@@ -2,80 +2,80 @@ var d;
 var startTime;
 var currentTime = d;
 var clockRunning = false;
-var timeElapsed = [0,0,0];
+var timeElapsed = [0, 0, 0];
 var timeElapsedRaw = 0;
 var timerInitial = 300; //300 = 5 Minutes
 
 
 //functions
-function clockRun(){
-    if(clockRunning == true){
-        updateClock();
-        currentTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
-        document.getElementById("current-time-display").innerHTML = currentTime.join(":");
-        updateTimeElapsed();
-        updateTimeLeft();
-    }
-    setTimeout(clockRun,1000); //runs once a second
+function clockRun() {
+  if (clockRunning == true) {
+    updateClock();
+    currentTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
+    document.getElementById("current-time-display").innerHTML = currentTime.join(":");
+    updateTimeElapsed();
+    updateTimeLeft();
+  }
+  setTimeout(clockRun, 1000); //runs once a second
 }
 
-function startClock(){
-    startTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
-    document.getElementById("start-time-display").innerHTML = startTime.join(":");
-    document.getElementById("current-time-display").innerHTML = startTime.join(":");
-    document.getElementById("time-elapsed-display").innerHTML = "0:0:0";
-    timeElapsedRaw = 0;
-    clockRunning = true;
+function startClock() {
+  startTime = [d.getHours(), d.getMinutes(), d.getSeconds()];
+  document.getElementById("start-time-display").innerHTML = startTime.join(":");
+  document.getElementById("current-time-display").innerHTML = startTime.join(":");
+  document.getElementById("time-elapsed-display").innerHTML = "0:0:0";
+  timeElapsedRaw = 0;
+  clockRunning = true;
 }
 
-
-
-function updateClock(){
-    d = new Date();
+function updateClock() {
+  d = new Date();
 }
 
-function updateTimeElapsed(){
-    var hours, minutes, seconds;
-    timeElapsedRaw ++; //increment every second
-    timeElapsed[2] = timeElapsedRaw%60;
-    timeElapsed[1] = Math.floor(timeElapsedRaw/60);
-    console.log(timeElapsedRaw);
-    console.log(timeElapsed);
-    document.getElementById("time-elapsed-display").innerHTML = timeElapsed.join(":");
+function updateTimeElapsed() {
+  var hours, minutes, seconds;
+  timeElapsedRaw++; //increment every second
+  timeElapsed[2] = timeElapsedRaw % 60;
+  timeElapsed[1] = Math.floor(timeElapsedRaw / 60);
+  console.log(timeElapsedRaw);
+  console.log(timeElapsed);
+  document.getElementById("time-elapsed-display").innerHTML = timeElapsed.join(":");
 }
 
-function updateTimeLeft(){
+function updateTimeLeft() {
   var timerCalc = timerInitial - timeElapsedRaw;
-  var timerFormat = [0,0,0];
+  var timerFormat = [0, 0, 0];
 
-  timerFormat[2] = timerCalc%60;
-  timerFormat[1] = Math.floor(timerCalc/60);
-  timerFormat[0] = Math.floor(timerCalc/3600);
+  timerFormat[2] = timerCalc % 60;
+  timerFormat[1] = Math.floor(timerCalc / 60);
+  timerFormat[0] = Math.floor(timerCalc / 3600);
   document.getElementById("time-left-display").innerHTML = timerFormat.join(":");
-  if(timerCalc <= 0){
+  if (timerCalc <= 0) {
     clockRunning = false;
     console.log("Time up.");
   }
 }
 
 //onload
-window.onload = function(){
+window.onload = function() {
   console.log("test");
   clockRun();
 };
 
 //event handlers
-document.getElementById("reset-button").onclick = function(){
+document.getElementById("reset-button").onclick = function() {
   timeElapsedRaw = 0;
-  timeElapsed = [0,0,0];
+  timeElapsed = [0, 0, 0];
   clockRunning = false;
   document.getElementById("start-time-display").innerHTML = "";
   document.getElementById("current-time-display").innerHTML = "";
-  document.getElementById("time-elapsed-display").innerHTML= timeElapsed.join(":");
+  document.getElementById("time-elapsed-display").innerHTML = timeElapsed.join(":");
   document.getElementById("time-left-display").innerHTML = "0:0:0";
 };
-document.getElementById("start-button").onclick = function(){
-    updateClock();
-    startClock();
+document.getElementById("start-button").onclick = function() {
+  updateClock();
+  startClock();
 };
-document.getElementById("stop-button").onclick = function () { clockRunning = false; };
+document.getElementById("stop-button").onclick = function() {
+  clockRunning = false;
+};
